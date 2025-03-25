02581edeb308ec0fb04b802248bb6ef674154ca1
Pawan Kumar <pawank@exeire.com>
2025-03-25 07:37:56 +0530

    Databinding Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [UserComponent, AdminComponent],
+  imports: [UserComponent, AdminComponent, DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
