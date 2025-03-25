e8346b0317498fcf36dc9bfffc862618df175786
Pawan Kumar <pawank@exeire.com>
2025-03-25 14:49:51 +0530

    Add Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [UserComponent, AdminComponent, DataBindingComponent],
+  imports: [NgIfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
