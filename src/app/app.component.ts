1dadf2216b68855b5556ba17c77dce49e573c84e
Pawan Kumar <pawank@exeire.com>
2025-03-31 10:09:26 +0530

    LOgin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { UserComponent } from './components/user/user.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
-import { NgIfComponent } from './components/ng-if/ng-if.component';
 
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports: [NgIfComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
